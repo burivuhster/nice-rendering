01cabd658ff44938b7408b35fe4764a0c5464a5d
Eugene <burivuh@gmail.com>
2020-04-10 15:12:08 +0300

    More performance optimizations

--- src/components/Editor.OneHalf.js
+++ src/components/Editor.OneHalf.js
@@ -3,49 +3,66 @@ import React from 'react'
 import { Grid, Paper, Box, IconButton, TextField } from '@material-ui/core'
 import { VolumeOff, Pause, VolumeUp, Mic, FlashAuto, GTranslate } from '@material-ui/icons'
 
-export default function OneHalfEditor({ direction, phrase, setAttr }) {
-  const directionReverse = direction === 'to' ? 'from' : 'to'
-  const lang = 'EN'
+// Чтобы избежать перерисовки иконок каждый раз, когда в инпут что-то вводят - вынес тулбар в отдельный компонент и обернул в HOC React.memo
+// Вынеси в отдельный файл если считаешь нужным
+const EditorToolbar = React.memo(function({ sound, track, micEnabled, translationEnabled }) {
+  return (
+    <Grid item container>
+      {!sound && (
+        <IconButton disabled>
+          <VolumeOff />
+        </IconButton>
+      )}
+      {sound && sound !== track && (
+        <IconButton>
+          <VolumeUp />
+        </IconButton>
+      )}
+      {sound && sound === track && (
+        <IconButton>
+          <Pause />
+        </IconButton>
+      )}
 
-  const sound = phrase[direction].sound
+      <IconButton color='default' disabled={!micEnabled} component={Box} ml='auto'>
+        <Mic />
+      </IconButton>
+      <IconButton disabled={!micEnabled}> {/* тут не знаю что за логика, возможно отдельным параметром рулить надо */}
+        <FlashAuto />
+      </IconButton>
 
-  const text = phrase[direction].text
-  const setText = text => setAttr(phrase._id, direction, 'text', text)
-  const textReverse = phrase[directionReverse].text
+      <IconButton disabled={!translationEnabled} component={Box} ml='auto'>
+        <GTranslate />
+      </IconButton>
+    </Grid>
+  )
+})
+
+// Чтобы избежать перерисовки одной половины при редактировании другой - порефакторил, чтобы сюда пропсами приходили только необходимые данные, и пропсы не менялись при редактировании другой половины (по крайней мере не на каждое нажатие клавиатуры)
+export default React.memo(function OneHalfEditor({
+  phraseId,
+  lang = 'en',
+  direction,
+  phraseHalf,
+  translationEnabled,
+  setAttr
+}) {
+  const sound = phraseHalf.sound
+
+  const text = phraseHalf.text
+  const setText = text => setAttr(phraseId, direction, 'text', text)
 
   const track = null
 
   return (
     <Paper component={Box} p={2}>
       <Grid container direction='column' spacing={1}>
-        <Grid item container>
-          {!sound && (
-            <IconButton disabled>
-              <VolumeOff />
-            </IconButton>
-          )}
-          {sound && sound !== track && (
-            <IconButton>
-              <VolumeUp />
-            </IconButton>
-          )}
-          {sound && sound === track && (
-            <IconButton>
-              <Pause />
-            </IconButton>
-          )}
-
-          <IconButton color='default' disabled={!text} component={Box} ml='auto'>
-            <Mic />
-          </IconButton>
-          <IconButton disabled={!text}>
-            <FlashAuto />
-          </IconButton>
-
-          <IconButton disabled={!textReverse} component={Box} ml='auto'>
-            <GTranslate />
-          </IconButton>
-        </Grid>
+        <EditorToolbar
+          sound={sound}
+          track={track}
+          micEnabled={!!text}
+          translationEnabled={translationEnabled}
+        />
 
         <Grid item container direction='column'>
           <TextField
@@ -61,4 +78,4 @@ export default function OneHalfEditor({ direction, phrase, setAttr }) {
       </Grid>
     </Paper>
   )
-}
+})
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
 import { Grid, Box, IconButton } from '@material-ui/core'
 import { Delete, Add } from '@material-ui/icons'
@@ -9,29 +9,60 @@ import OneHalfEditor from './Editor.OneHalf'
 // Без нее реакт для функциональных компонентов (также как и для компонентов-классов) вызывает рендер снова, независимо от того, поменялись ли пропсы
 // React.memo - это для функциональных компонентов как React.PureComponent для компонентов-классов)
 export default React.memo(function Editor({ phrase, onAddNextTo, setAttr, onDelete }) {
-  const onDeleteClick = useCallback(() => { onDelete(phrase._id) }, [onDelete, phrase]);
-  const onAddNextClick = useCallback(() => { onAddNextTo(phrase._id) }, [onAddNextTo, phrase]);
+  const onDeleteClick = useCallback(() => {
+    onDelete(phrase._id)
+  }, [onDelete])
+  const onAddNextClick = useCallback(() => {
+    onAddNextTo(phrase._id)
+  }, [onAddNextTo])
 
-  return (
-    <div>
+  // Увидел в профайлере, что перерисовка иконок много времени занимает, обернул их тоже в useMemo -
+  const topBar = useMemo(
+    () => (
       <Grid container direction='row-reverse'>
         <IconButton onClick={onDeleteClick} color='secondary'>
           <Delete />
         </IconButton>
       </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <OneHalfEditor direction='from' phrase={phrase} setAttr={setAttr} />
-        </Grid>
-        <Grid item xs={6}>
-          <OneHalfEditor direction='to' phrase={phrase} setAttr={setAttr} />
-        </Grid>
-      </Grid>
+    ),
+    [onDeleteClick]
+  )
+
+  const bottomBar = useMemo(
+    () => (
       <Box m={1}>
         <IconButton onClick={onAddNextClick} color='primary' style={{ position: 'absolute' }}>
           <Add />
         </IconButton>
       </Box>
+    ),
+    [onAddNextClick]
+  )
+
+  return (
+    <div>
+      {topBar}
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <OneHalfEditor
+            direction='from'
+            phraseId={phrase._id}
+            phraseHalf={phrase.from}
+            setAttr={setAttr}
+            translationEnabled={!!phrase.to.text}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <OneHalfEditor
+            direction='to'
+            phraseId={phrase._id}
+            phraseHalf={phrase.to}
+            setAttr={setAttr}
+            translationEnabled={!!phrase.from.text}
+          />
+        </Grid>
+      </Grid>
+      {bottomBar}
     </div>
   )
 })
